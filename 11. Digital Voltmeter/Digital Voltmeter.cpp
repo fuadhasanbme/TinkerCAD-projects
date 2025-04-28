adeb46d9701e1cf4a37be2c3b9cdc513d184f1dc
Md. Fuad Hasan Hamim <hamim2215029@stud.kuet.ac.bd>
2025-04-28 11:54:25 +0600

    Add files via upload

---
+++ 11. Digital Voltmeter/Digital Voltmeter.cpp
@@ -0,0 +1,43 @@
+/*	Code written by-
+Fuad Hasan 
+BME, KUET	*/
+
+/*Digital Voltmeter using Arduino*/
+
+
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+float input_voltage = 0.0;
+float temp = 0.0;
+float r1 = 1000000.0; //1 M ohm
+float r2 = 10000.0;  //1k ohm
+
+
+void setup()
+{
+  Serial.begin(9600);
+  lcd.begin(16,2);
+  lcd.print("DIGITAL VOLTMETER ");
+}
+
+void loop()
+{
+
+  //Conversion formula for voltage
+
+  float analog_value = analogRead(A0);
+  temp = (analog_value * 5.0) / 1024.0;
+  input_voltage= temp/(r2/(r2+r1));
+
+  if (input_voltage < 0.1)
+  {
+    input_voltage = 0.0;
+  }
+  Serial.print("v = ");
+  Serial.println(input_voltage);
+  lcd.setCursor(0,1);
+  lcd.print("Voltage= ");
+  lcd.print(input_voltage);
+  delay(1000);
+}
